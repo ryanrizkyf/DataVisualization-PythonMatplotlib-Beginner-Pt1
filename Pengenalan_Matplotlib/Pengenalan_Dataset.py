bf86c17d575c95c4d8a89205bc9d668ebe6e6652
Ryan <ryanrizkyf@gmail.com>
2021-02-18 09:30:56 +0700

    edit commit

--- Pengenalan_Matplotlib/Pengenalan_Dataset.py
+++ Pengenalan_Matplotlib/Pengenalan_Dataset.py
@@ -8,3 +8,17 @@ print('Lima data teratas:')
 print(dataset.head())
 
 # Di sini terlihat bahwa kita punya 5000 barisdata, dan terdiri dari 9 kolom.
+
+# Penjelasan dataset  data transaksi supermarket tahun 2019 :
+# order_id : ID dari order/transaksi, 1 transaksi bisa terdiri dari beberapa produk,
+# tetapi hanya dilakukan oleh 1 customer
+# order_date : tanggal terjadinya transaksi
+# customer_id : ID dari pembeli, bisa jadi dalam satu hari, 1 customer melakukan transaksi beberapa kali
+# city : kota tempat toko terjadinya transaksi
+# province : provinsi (berdasarkan city)
+# product_id : ID dari suatu product yang dibeli
+# brand : brand/merk dari product. Suatu product yang sama pasti memiliki brand yang sama
+# quantity : Kuantitas / banyaknya product yang dibeli
+# item_price : Harga dari 1 product (dalam Rupiah). Suatu product yang sama,
+# bisa jadi memiliki harga yang berbeda saat dibeli
+# Gross Merchandise Value atau total uang yang dibayarkan customer
