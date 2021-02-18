7cf61f2c3b8ec7c65a216f2ff5bda3308ba8a106
Ryan <ryanrizkyf@gmail.com>
2021-02-18 10:30:02 +0700

    edit commit

---
+++ Kustomisasi_Grafik_Pt_Satu/Mengubah_Figure_Size.py
@@ -0,0 +1,14 @@
+# dataset = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+dataset = pd.read_csv(
+    'retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(
+    lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
