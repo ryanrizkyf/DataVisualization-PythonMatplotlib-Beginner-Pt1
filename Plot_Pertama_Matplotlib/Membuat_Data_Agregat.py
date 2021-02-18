d10cec82fd943a4216aa9440b2781f740d90e3ae
Ryan <ryanrizkyf@gmail.com>
2021-02-18 09:52:56 +0700

    edit commit

---
+++ Plot_Pertama_Matplotlib/Membuat_Data_Agregat.py
@@ -0,0 +1,13 @@
+# dataset = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv
+
+import datetime
+import pandas as pd
+dataset = pd.read_csv(
+    'retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(
+    lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+print('Ukuran dataset: %d baris dan %d kolom\n' % dataset.shape)
+print(dataset.head())
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+print(monthly_amount)
