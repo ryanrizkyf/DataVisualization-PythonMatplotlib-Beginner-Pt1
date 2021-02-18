f0daf9d51f6632ae71fc66e01616e58324686ac0
Ryan <ryanrizkyf@gmail.com>
2021-02-18 15:53:35 +0700

    edit commit

---
+++ Kustomisasi_Grafik_Pt_Satu/Kustomisasi_Grid.py
@@ -0,0 +1,24 @@
+# Kustomisasi grid berguna agar nilai di masing-masing titik bisa mudah dipetakan ke sumbu-y.
+# Caranya dengan menambahkan plt.grid() dengan parameter yang mirip dengan line.
+
+# dataset = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+dataset = pd.read_csv(
+    'retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(
+    lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(
+    color='green', marker='o', linestyle='-.', linewidth=2)
+plt.title('Monthly GMV Year 2019', loc='center',
+          pad=40, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.show()
