35cdc9783e97f5fb64e330773b6d7f59409a18d2
Ryan <ryanrizkyf@gmail.com>
2021-02-18 11:04:35 +0700

    edit commit

--- Kustomisasi_Grafik_Pt_Satu/Mengubah_Figure_Size.py
+++ Kustomisasi_Grafik_Pt_Satu/Mengubah_Figure_Size.py
@@ -9,6 +9,9 @@ dataset['order_month'] = dataset['order_date'].apply(
     lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
 monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+
+# Dalam contoh ini, ukuran Figure telah diubah agar memiliki panjang 15 inch, dan lebar 5 inch.
+# Sehingga line chart-nya bisa memanjang ke kanan dan lebih mudah dilihat trend-nya.
 plt.figure(figsize=(15, 5))
 dataset.groupby(['order_month'])['gmv'].sum().plot()
 plt.show()
